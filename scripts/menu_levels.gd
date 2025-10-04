1ed48b02b8e2668b3b3f56719064e1ee2fcad190
Mohammed237 <mohammedmostafa2372002@gmail.com>
2025-10-04 14:20:15 +0300

    add quit button to menu levels

--- scripts/menu_levels.gd
+++ scripts/menu_levels.gd
@@ -5,6 +5,7 @@ extends Control
 @onready var level2 = $"levels_container/Level2"
 @onready var level3 = $"levels_container/Level3"
 @onready var level4 = $"levels_container/Level4"
+@onready var quit_button = $VBoxContainer/Quit
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,6 +18,7 @@ func _ready():
 	level2.connect("pressed", Callable(self, "_on_level_2_button_pressed"))
 	level3.connect("pressed", Callable(self, "_on_level_3_button_pressed"))
 	level4.connect("pressed", Callable(self, "_on_level_4_button_pressed"))
+	quit_button.pressed.connect(_on_quit_button_pressed)
 
 func _on_level_1_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/Level 1.tscn")
@@ -29,3 +31,6 @@ func _on_level_3_button_pressed():
 
 func _on_level_4_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/Level 4.tscn")
+
+func _on_quit_button_pressed():
+	get_tree().quit()
